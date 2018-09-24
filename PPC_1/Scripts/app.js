3876613c704185183c3a2b2b0422e53cab76b563
jonathan.vieira <jonathan.vieira@cebraspe.org.br>
2018-09-24 18:08:44 -0300

    usando js para manipular select

--- PPC_1/Scripts/app.js
+++ PPC_1/Scripts/app.js
@@ -8,7 +8,29 @@ function AdicionarReadonly() {
     $('input').add('readonly');
 }
 
-function PreencherDadosCoordenador(CPF) {
-    $('#preencheauto').prop('placeholder', CPF);
-    //$('.preencheauto1').add('placeholder', MaiorTitulacao);
-}
+//function PreencherDadosCoordenador(CPF) {
+//    $('#preencheauto').val(CPF);
+//    //$('.preencheauto1').add('placeholder', MaiorTitulacao);
+//}
+
+//java script puro 
+//function PreencherDadosCoordenador() {
+//    var x = document.getElementById("selectCoordenador").value;
+//    document.getElementById("preencheauto").value = x;
+
+//}
+
+//Sem mostrar a função no html
+//var x = document.getElementById("selectCoordenador");
+//x.onchange = function () {
+//document.getElementById("preencheauto").value = x.value;
+//}
+
+$("#selectCoordenador").change(function () {
+    var slt = "";
+    $("option[value]").each(function () {
+        slt += $(this).text() + " ";
+    });
+    $("#preencheauto").val(slt); 
+})
+    
