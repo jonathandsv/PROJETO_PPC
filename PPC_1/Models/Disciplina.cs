fbc89dc0d1d35aeaafd21d28e17d6229a91923a2
jonathan.vieira <jonathan.vieira@cebraspe.org.br>
2018-10-08 17:26:14 -0300

    ppc

---
+++ PPC_1/Models/Disciplina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPC_1.Models
+{
+    public class Disciplina
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public int Semestre { get; set; }
+        public int IdCurso { get; set; }
+    }
+}
