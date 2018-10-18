aa6ba06b47fc07844e26bb9397354a36cd050689
jonathan.vieira <jonathan.vieira@cebraspe.org.br>
2018-10-18 17:56:22 -0300

    Tela Cadastro de Bibliografia

---
+++ PPC_1/Models/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPC_1.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Edicao { get; set; }
+        public string ISBN { get; set; }
+        public int Ano { get; set; }
+        public string Editora { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+    }
+}
